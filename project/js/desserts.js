23e17e0ae6b82317c8710b90f2b976e8cb0bc32c
nicolew88 <nicolew88@nycstudents.net>
2023-11-14 13:12:49 -0500

    .

---
+++ project/js/desserts.js
@@ -0,0 +1,38 @@
+const desserts = [
+    {
+       name: "Red Velvet",
+       class: "cake",
+    },
+    {
+        name: "",
+        class: "cake",
+    },
+    {
+        name: "",
+        class: "cake",
+    },
+    {
+        name: "",
+        class: "cake",
+    },
+    {
+        name: "Chocolate Chip",
+        class: "cookie",
+    },
+    {
+        name: "Mint Chocolate",
+        class: "ice cream",
+    },
+    {
+        name: "Brown Sugar Milk Tea",
+        class: "drink",
+    },
+    {
+        name: "Classic Beignet",
+        class: "pastry",
+    },
+    {
+        name: "",
+        class: "pastry",
+    },
+]
